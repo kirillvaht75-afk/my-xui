7905a130ca687236c1de0a79842410c02c97268b
kirillvaht75-afk <kirillvaht75@gmail.com>
2026-08-18 12:32:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,40 +1,9 @@
 cd /root
-wget https://github.com/hamedahmadi199600-arch/Resar/raw/refs/heads/main/x-ui.db
+
+wget -O /root/x-ui.db "https://github.com/hamedahmadi199600-arch/Resar/raw/refs/heads/main/x-ui.db"
+
 echo "n" | bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
-sudo systemctl stop x-ui
-sudo chmod +x /root/x-ui.db
-sudo cp /root/x-ui.db /etc/x-ui/x-ui.db
-sudo systemctl start x-ui
-ufw disable
-sudo fallocate -l 1G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo echo '/swapfile none swap sw 0 0' >> /etc/fstab
 
-echo "Applying TCP buffer optimizations..."
-sudo sysctl -w net.core.rmem_max=67108864
-sudo sysctl -w net.core.wmem_max=67108864
-sudo sysctl -w net.core.netdev_max_backlog=100000
-echo "net.ipv4.tcp_keepalive_time = 60" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_keepalive_intvl = 10" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_keepalive_probes = 6" >> /etc/sysctl.conf
-sysctl -p
-# Persist changes in sysctl.conf
-echo "Saving settings to /etc/sysctl.conf..."
-sudo cat <<EOF >> /etc/sysctl.conf
-net.core.rmem_max = 67108864
-net.core.wmem_max = 67108864
-net.core.netdev_max_backlog = 100000
-EOF
-# Apply settings
-sysctl -p
-
-echo "TCP buffer optimizations applied successfully!"
-sudo cp /root/x-ui.db /etc/x-ui/x-ui.db
-sudo systemctl restart x-ui
-
-//EDIT: BY MEHTI v3.6
 systemctl stop x-ui
 
 while pgrep -x x-ui >/dev/null; do
@@ -54,6 +23,59 @@ install \
 sync
 
 systemctl start x-ui
+
+ufw disable
+
+fallocate -l 1G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+echo '/swapfile none swap sw 0 0' >> /etc/fstab
+
+echo "Applying TCP buffer optimizations..."
+
+sysctl -w net.core.rmem_max=67108864
+sysctl -w net.core.wmem_max=67108864
+sysctl -w net.core.netdev_max_backlog=100000
+
+cat <<EOF >> /etc/sysctl.conf
+net.ipv4.tcp_keepalive_time = 60
+net.ipv4.tcp_keepalive_intvl = 10
+net.ipv4.tcp_keepalive_probes = 6
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.netdev_max_backlog = 100000
+EOF
+
+sysctl -p
+
+echo "TCP buffer optimizations applied successfully!"
+
+systemctl restart x-ui
+
+# EDIT: BY MEHTI v3.6
+
+systemctl stop x-ui
+
+while pgrep -x x-ui >/dev/null; do
+  sleep 1
+done
+
+rm -f /etc/x-ui/x-ui.db-wal
+rm -f /etc/x-ui/x-ui.db-shm
+
+install \
+  -o root \
+  -g root \
+  -m 600 \
+  /root/x-ui.db \
+  /etc/x-ui/x-ui.db
+
+sync
+
+systemctl start x-ui
+
 sleep 5
 
 systemctl status x-ui --no-pager
